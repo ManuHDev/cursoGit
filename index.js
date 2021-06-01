d9af21b8858f5f80f79f3260c6b7e7d6ac466cf1
MHamana <mhamana@strappinc.net>
2021-05-31 22:04:34 -0400

    nuevos archivos

---
+++ index.js
@@ -0,0 +1,33 @@
+var modal = $('#modal');
+$(function(){
+  $('img').click(function(){
+    var idImg = $(this).attr("id");
+    //console.log(idImg);
+    var srcImg = $(this).attr("src");
+    //console.log(srcImg);
+    showModal(idImg,srcImg);
+  });
+  hideModal();
+  /*Navbar*/
+  $('.fa-bars').click(function(){
+      $('nav ul li').addClass("show");
+      $('.fa-times').css("display","block");
+      $('.fa-bars').css("display","none");
+  });
+  $('.fa-times').click(function(){
+    $('nav ul li').removeClass("show");
+    $('.fa-times').css("display","none");
+    $('.fa-bars').css("display","block");
+  });
+});
+function showModal(idImg,srcImg){
+    var imagen = idImg;
+    var src = srcImg;
+    $('#imgModal').attr("src",src);
+    modal.css("display","block");
+}
+function hideModal(){
+    $('.cerrar').click(function(){
+        modal.css("display","none");
+    });
+}
